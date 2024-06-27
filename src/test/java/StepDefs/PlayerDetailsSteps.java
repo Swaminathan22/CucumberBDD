c2bf47124f21af6589d275486531bef7dfe013c7
Swaminathan <swami.aug22@gmail.com>
2024-06-27 15:03:02 +0530

    Resolved merge conflicts

--- src/test/java/StepDefs/PlayerDetailsSteps.java
+++ src/test/java/StepDefs/PlayerDetailsSteps.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import org.assertj.core.api.Assertions;
 import org.junit.Assert;
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
@@ -22,45 +23,55 @@ import io.cucumber.java.en.When;
 public class PlayerDetailsSteps {
 
     WebDriver driver;
-
+    WebDriverWait wait;
     @Given("I launch ESPN Cricinfo website")
     public void i_launch_espn_cricinfo_website() {
         driver = new ChromeDriver();
         driver.get("https://www.espncricinfo.com/");
         driver.manage().window().maximize();
-        handleOverlay();
+        //handleOverlay(15);
     }
 
     @When("I navigate to the {string} section under {string}")
-    public void i_navigate_to_the_section_under(String section, String menu) {
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+    public void i_navigate_to_the_section_under(String section, String menu) throws InterruptedException {
+		
+		/*
+		 * wait = new WebDriverWait(driver, Duration.ofSeconds(5)); Thread.sleep(15000);
+		 * if(wait.until(ExpectedConditions.visibilityOfElementLocated(By.id(
+		 * "wzrk_wrapper"))) != null) { WebElement overlayInfoButton
+		 * =driver.findElement(By.xpath(
+		 * "//div[@id='wzrk_wrapper']//button[@id='wzrk-cancel']"));
+		 * overlayInfoButton.click(); }
+		 */		 
+    	
         try {
-            handleOverlay();
-            Assertions.assertThat(driver.findElement(By.cssSelector("a[title='" + menu + "']")).isDisplayed())
+            handleOverlay(15); 
+            Assertions.assertThat(driver.findElement(By.cssSelector("a[title='"+menu+"']")).isDisplayed())
                 .as("Check if " + menu + " section is displayed").isTrue();
         } catch (Exception e) {
             e.printStackTrace();
             Assertions.fail(menu + " section is not displayed or could not be found", e);
         }
-        WebElement overlayInfoButton = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[@id='wzrk-cancel']")));
-        overlayInfoButton.click();
+       /* WebElement overlayInfoButton = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[@id='wzrk-cancel']")));
+        overlayInfoButton.click();*/
         Actions act = new Actions(driver);
-        act.moveToElement(driver.findElement(By.cssSelector("a[title='" + menu + "']"))).build().perform();
+        act.moveToElement(driver.findElement(By.cssSelector("a[title='"+menu+"']"))).build().perform();
         driver.findElement(By.xpath("//span[contains(text(),'" + section + "')]")).click();
-        handleOverlay();
+        handleOverlay(15);
+    	
     }
 
     @When("I select the player named {string}")
     public void i_select_the_player_named(String playerName) {
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
-        WebElement targetElement = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//span[contains(text(),'" + playerName + "')]")));
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement targetElement = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//span[contains(text(),'"+playerName+"')]")));
 
         ((JavascriptExecutor) driver).executeScript("window.scrollBy(0, 500);");
         ((JavascriptExecutor) driver).executeScript("arguments[0].style.border='3px solid red'", targetElement);
-        handleOverlay();
+        handleOverlay(15);
         
-        driver.findElement(By.xpath("//span[contains(text(),'" + playerName + "')]")).click();
-        handleOverlay();
+        driver.findElement(By.xpath("//span[contains(text(),'"+playerName+"')]")).click();
+       // handleOverlay(15);
     }
 
     @Then("I should see the player details and verify them against the expected data")
@@ -86,12 +97,33 @@ public class PlayerDetailsSteps {
         driver.quit();
     }
 
-    public void handleOverlay() {
+    public void handleOverlay(int seconds) {
         try {
-            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
-            WebElement overlayCloseButton = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[@class='sprite close']")));
-            overlayCloseButton.click();
-            System.out.println("Overlay closed successfully.");
+        	boolean advertisement= false;
+        	boolean notification= false;
+        	
+            wait = new WebDriverWait(driver, Duration.ofSeconds(seconds));
+            /*WebElement overlayCloseButton = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[@class='sprite close']")));*/
+        	advertisement=driver.findElement(By.xpath("//iframe[contains(@id, 'google_ads_iframe')]")).isDisplayed();
+        	notification=driver.findElement(By.id("wzrk-cancel")).isDisplayed();
+        	
+        	
+			if(advertisement)
+        	{	
+        		driver.switchTo().frame(driver.findElement(By.xpath("//iframe[contains(@id, 'google_ads_iframe')]")));
+        		WebElement overlayCloseButton=driver.findElement(By.xpath("//a[@class='sprite close']"));
+        		overlayCloseButton.click();
+        		driver.switchTo().defaultContent();
+        		System.out.println("Overlay closed successfully.");
+        	}
+			else if(notification)
+			{
+				WebElement overlayInfoButton=driver.findElement(By.xpath("//div[@id='wzrk_wrapper']//button[@id='wzrk-cancel']"));
+		   		 overlayInfoButton.click(); 
+			}
+        	else
+        		System.out.println("Overlay did not appear or was already closed.");
+            
         } catch (Exception e) {
             System.out.println("Overlay did not appear or was already closed.");
         }
